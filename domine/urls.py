9eb90ccc24657f7c16978d1a6eec4a1f39f33bf1
Luciano Muñoz <luchisds@gmail.com>
2016-08-02 03:13:27 -0300

    Changes in layout and dropdown nav

--- domine/urls.py
+++ domine/urls.py
@@ -20,5 +20,6 @@ from website import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='home')
+    url(r'^$', views.index, name='home'),
+    url(r'^escuela/$', views.school, name='escuela'),
 ]
