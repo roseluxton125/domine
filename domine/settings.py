9674315233396939c57b1879cfc6a72854c8bf7c
Luciano Muñoz <luchisds@gmail.com>
2016-09-12 13:40:42 -0300

    key django changes

--- domine/settings.py
+++ domine/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vvpk%qqr_kfwhj(9(8#!2929a@wxwh^j!qhj=q^ed*0@r+mi@('
+with open(BASE_DIR+'/diputados/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
